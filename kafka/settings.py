af300e40a71b4fa82e2bd8242caceaa1d442d48a
Akshay <akshay@tyloones.com>
2024-05-24 19:46:35 +0530

    update

--- kafka/settings.py
+++ kafka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&k4xw&^d%fso+&6$bzwj^7=t0p*_vd5+*w4e7n@v@^m=f=2fn&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.0.103', 'localhost', '127.0.0.1']
 
 
 # Application definition
